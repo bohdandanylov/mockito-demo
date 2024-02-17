ac8855ed5397659e1d8eab66caf6d287d77096a9
Bohdan Danylov <bohdandanylov@gmail.com>
2024-02-16 21:10:32 -0800

    v1.0 - Setting up a Spring Boot Project

---
+++ src/test/java/learnspringboot/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package learnspringboot.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
